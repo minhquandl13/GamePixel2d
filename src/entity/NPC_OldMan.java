1f5115b175d7d5bacf521f0d743a444311ca487a
quocthai2612003 <92153923+quocthai2612003@users.noreply.github.com>
2023-03-06 20:46:12 +0700

    UP

--- src/entity/NPC_OldMan.java
+++ src/entity/NPC_OldMan.java
@@ -30,7 +30,7 @@ public class NPC_OldMan extends Entity {
         dialogues[0] = "Hello, lad.";
         dialogues[1] = "So you've come to this island to find the\ntreasure?";
         dialogues[2] = "I used to be a great wizard but now...\nI'm a bit too old for taking an adventure.";
-        dialogues[3] = "This place has 1 final boss so beware of him,\nhis name QUOC DIRTY OLD MAN.";
+        dialogues[3] = "This place has 1 final boss so beware of him,\nhis name QUAN HAY QUEN.";
         dialogues[4] = "Well, good luck on you.";
     }
 
