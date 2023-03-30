332db43eee8b8c1b619dc087ed3241d1ea0d05f6
NhatBao03 <21130280@st.hcmuaf.edu.vn>
2023-03-31 00:59:38 +0700

    update type_obstacle

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -105,6 +105,7 @@ public class Entity {
     public final int type_shield = 5;
     public final int type_consumable = 6;
     public final int type_pickupOnly = 7;
+    public final int type_obstacle = 8;
 
 
     public Entity(GamePanel gp) {
--- src/object/OBJ_Chest.java
+++ src/object/OBJ_Chest.java
@@ -7,11 +7,54 @@ import javax.imageio.ImageIO;
 import java.io.IOException;
 
 public class OBJ_Chest extends Entity {
+	
+	GamePanel gp;
+	Entity loot;
+	boolean opened = false;
+	
     public OBJ_Chest(GamePanel gp) {
         super(gp);
-
+        this.gp = gp;
+        this.loot = loot;
+        
+        type = type_obstacle;
         name = "Chest";
-        down1 = setup("/Object/chest", gp.tileSize, gp.tileSize);
+        image = setup("/Object/chest", gp.tileSize, gp.tileSize);
+        image2 = setup("/Object/chest_opened", gp.tileSize, gp.tileSize);
+        down1 = image;
+        collision = true;
         price = 25;
+        
+        solidArea.x = 4;
+        solidArea.y = 16;
+        solidArea.width = 40;
+        solidArea.height = 32;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+    }
+    public void interac() {
+    	
+    	gp.gameState = gp.dialogueState;
+    	
+    	if(opened == false) {
+    		gp.playSE(3);
+    		
+    		StringBuilder sb = new StringBuilder();
+    		sb.append("You open the chest and find a " + loot.name + "!");
+    		
+    		if(gp.player.inventory.size() == gp.player.maxIventorySize) {
+    			sb.append("\n...But you cannot catty any more!");
+    		}
+    		else {
+    			sb.append("\n You abtain the " + loot.name + "!");
+    			gp.player.inventory.add(loot);
+    			down1 = image2;
+    			opened = true;
+    		}
+    		gp.ui.currentDiaglog = sb.toString();
+    	}
+    	else {
+    		gp.ui.currentDiaglog = "It's empty";
+    	}
     }
 }
