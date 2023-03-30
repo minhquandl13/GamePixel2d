6f61355f64b95a0c7eecd6ffc1ee6a25bbecc643
NhatBao03 <21130280@st.hcmuaf.edu.vn>
2023-03-31 00:57:07 +0700

    update use

--- src/object/OBJ_Potion_Red.java
+++ src/object/OBJ_Potion_Red.java
@@ -18,11 +18,12 @@ public class OBJ_Potion_Red extends Entity {
         price = 20;
     }
 
-    public  void use(Entity entity){
+    public  boolean use(Entity entity){
         gp.gameState = gp.dialogueState;
         gp.ui.currentDiaglog = "You drink the " + name + "!\n"
                 + "Your life has been recovered by " + value + ".";
         entity.life += value;
         gp.playSE(2);
+        return true;
     }
 }
